9077758fd0353b45ee719e2306676753260b9a9f
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-26 23:25:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get 'name' do
-    "My name is _"
+    "M/y name is _"
   end
 
-  get 'hometown' do
+  get '/hometown' do
     "My hometown is _"
   end
 
-  get 'favorite-song' do
+  get '/favorite-song' do
     "My favorite song is _"
   end
 
